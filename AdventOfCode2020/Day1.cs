d74fdb9686194fad61e72bdf1c113787aefd63de
Natasha <lathamnatasha@gmail.com>
2020-12-01 22:26:28 +0100

    Add Solution part 2

--- AdventOfCode2020/Day1.cs
+++ AdventOfCode2020/Day1.cs
@@ -7,7 +7,13 @@ namespace AdventOfCode2020
 {
     public static class Day1
     {
-        public static void SolutionPart1()
+        public static void Solutions()
+        {
+            SolutionPart1();
+            SolutionPart2();
+        }
+
+        private static void SolutionPart1()
         {
             var content = ReadFile();
 
@@ -29,21 +35,30 @@ namespace AdventOfCode2020
             Console.WriteLine("Result: " + result.ToString("N0"));
         }
 
-        public static void SolutionPart2()
+        private static void SolutionPart2()
         {
             var content = ReadFile();
 
             var result = 0;
-            foreach (var number in content)
+            for (int i = 0; i < content.Length; i++)
             {
-                var searchNumber = 2020 - number;
-
-                var found = Array.Find(content, x => x == searchNumber);
-
-                if (found != 0)
+                for (int j = i + 1; j < content.Length; j++)
                 {
-                    Console.WriteLine($"Found it: {number} + {found}");
-                    result = number * found;
+                    var searchNumber2 = 2020 - content[i] - content[j];
+
+                    var found = Array.Find(content, x => x == searchNumber2);
+
+                    if (found != 0)
+                    {
+                        Console.WriteLine($"Found it: {content[i]} + {content[j]} + {found} equal to {content[i] + content[j] + found}");
+                        result = content[i] * content[j] * found;
+                        break;
+                    }
+
+                }
+
+                if(result != 0)
+                {
                     break;
                 }
             }
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            Day1.SolutionPart1();
+            Day1.Solutions();
         }      
     }
 }
